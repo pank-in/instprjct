c4d7a4d7ca635237dacfe5d5aa313655a332f36b
pank-in <pankinalex93@ukr.net>
2016-12-16 19:41:55 +0200

    first commit

---
+++ src/js/vendor.js
@@ -0,0 +1,6 @@
+//=require 'page/page.js'
+//=require 'jquery/dist/jquery.js'
+//=require 'bootstrap/dist/js/bootstrap.js'
+//=require 'firebase/firebase.js'
+//=require 'handlebars/dist/handlebars.runtime.js'
+//=require 'caman/dist/caman.full.js'
